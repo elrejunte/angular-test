8228e5aa9d88898e9d098fcae8533adda7608e5d
Marce barusso <marcelo.barusso@icbc.com.ar>
2019-08-22 13:07:45 -0300

    primera version prueba técnica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CountryService } from './services/country.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [CountryService]
 })
 export class AppModule { }
